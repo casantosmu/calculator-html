65b1e728363130ee15690993364d8d9c859822c3
Carlos <casantosmu@gmail.com>
2022-06-26 22:36:03 +0200

    Improved display manipulation with the keyboard

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ const keyboardElement = document.querySelector(".js-keyboard");
 
 keyboardElement.addEventListener("click", e => {
     const keyData = e.target.closest("[data-key]").dataset.key;
-    const enteredValuesContent = enteredValuesElement.textContent;
+    // const enteredValuesContent = enteredValuesElement.textContent;
     const currentValueContent = currentValueElement.textContent;
     if (!isNaN(keyData)) {
-        if (enteredValuesContent || +currentValueContent) {
-            currentValueElement.textContent += keyData;
-        } else {
+        if (currentValueContent === "0" && !currentValueContent.includes(".")) {
             currentValueElement.textContent = keyData;
         }
+        else if (currentValueContent.length < 13) {
+            currentValueElement.textContent += keyData;
+        }
     }
-    if (keyData === "decimal" && !currentValueContent.includes(".")) currentValueElement.textContent += ".";
-    if (keyData === "clear") currentValueElement.textContent = currentValueContent.slice(0, -1);
+    else if (keyData === "decimal" && !currentValueContent.includes(".")) {
+        currentValueElement.textContent += ".";
+    }
+    else if (keyData === "clear") {
+        currentValueElement.textContent = currentValueContent.slice(0, -1);
+        if (currentValueElement.textContent === "") currentValueElement.textContent = 0;
+    }
 })
