c56818c74e8034994bbb024e731f0701360f3c86
Carlos <casantosmu@gmail.com>
2022-06-26 13:36:30 +0200

    Added DOM selectors and display manipulation with the keyboard

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+const enteredValuesElement = document.querySelector(".js-entered-values");
+const currentValueElement = document.querySelector(".js-current-value");
+const keyboardElement = document.querySelector(".js-keyboard");
+
+keyboardElement.addEventListener("click", e => {
+    const keyData = e.target.closest("[data-key]").dataset.key;
+    const enteredValuesContent = enteredValuesElement.textContent;
+    const currentValueContent = currentValueElement.textContent;
+    if (!isNaN(keyData)) {
+        if (enteredValuesContent || +currentValueContent) {
+            currentValueElement.textContent += keyData;
+        } else {
+            currentValueElement.textContent = keyData;
+        }
+    }
+    if (keyData === "decimal" && !currentValueContent.includes(".")) currentValueElement.textContent += ".";
+    if (keyData === "clear") currentValueElement.textContent = currentValueContent.slice(0, -1);
+})
